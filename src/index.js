6efd9d97bcea1bd230a7b7b68ad6e2b0d5354538
sadness_ojisan <sadness.ojisan@gmail.com>
2022-08-24 02:07:28 +0900

    load audio

--- src/index.js
+++ src/index.js
@@ -8,14 +8,24 @@ server.get("/", async (request, reply) => {
 });
 
 server.get("/file", async (request, reply) => {
-  const res = await fetch("http://127.0.0.1:8080");
-  console.log(res);
-  const data = await res.blob();
+  const res = await fetch("http://127.0.0.1:8000");
+
+  const data = await res.arrayBuffer();
   reply.type(data.type);
-  reply.send(Buffer.from(await data.arrayBuffer()));
+  reply.headers({
+    "content-disposition": "attachment; filename=aaa.mp3",
+  });
+  reply.send(Buffer.from(data));
 });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.get("/html", async (request, reply) => {
+  reply.type("text/html");
+  reply.send(
+    "<html><body><audio src='http://127.0.0.1:8000/file' controls /></body></html>"
+  );
+});
+
+server.listen({ port: 8000 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
